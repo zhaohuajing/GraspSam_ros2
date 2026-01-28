6b95d0b6ace3ce4052dc88841c7e078ad198ecab
zhaohuajing <huajing_zhao@berkely.edu>
2026-01-27 22:43:24 -0500

    Added functional eval_edited_rgbd_noMask.py to load and inference with customer rgbd images (color+depth), in process of providing customer image + label in current eval.py

--- graspsam_docker/run_docker.sh
+++ graspsam_docker/run_docker.sh
@@ -18,7 +18,7 @@ docker run --rm -it \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   -v "$HOME/graspnet_ws:/root/graspnet_ws" \
-  -v "/media/csrobot/Data/Datasets/Grasp-Anything:/media/Grasp-Anything:ro" \
+  -v "/media/csrobot/Data1/Datasets/Grasp-Anything:/media/Grasp-Anything:ro" \
   -w /root/graspnet_ws/src/graspsam_ros2/graspsam_docker \
   ${IMAGE_NAME} #\
 # bash -lc "[ -f setup_runtime.sh ] && bash setup_runtime.sh || true; exec bash"
